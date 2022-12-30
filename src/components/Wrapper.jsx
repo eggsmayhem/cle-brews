944df5292b4637cefb35d76aad90244618515242
Sabrina Pyles <flipmybit@protonmail.com>
2022-12-29 19:26:02 -0500

    switched API call from local to remote

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -26,8 +26,8 @@ export default function Wrapper() {
 
     useEffect(() => {
         const fetchBreweries = async () => {
-            const breweriesList = await axios.get('http://127.0.0.1:3001/breweries/overview');
-            // const breweriesList = await axios.get('https://cleveland-brews-api.onrender.com/breweries/overview');
+            // const breweriesList = await axios.get('http://127.0.0.1:3001/breweries/overview');
+            const breweriesList = await axios.get('https://cleveland-brews-api.onrender.com/breweries/overview');
             const breweriesArray = breweriesList.data;
             setBreweries(breweriesArray);
         }
